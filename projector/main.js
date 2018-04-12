1c97aa765d3e0a2d7b13b344092f1321dfee425f
Marte <Marte@Martes-MacBook-Pro-3.local>
2018-04-12 12:51:28 +0200

    comments in js plus styling

--- projector/main.js
+++ projector/main.js
@@ -1,22 +1,29 @@
+// Image Canvas object
 const imagesAndCoord = [
     {canvasName: 'canvas', coords: [[650, 134], [794, 161], [656, 441], [514, 402]]},
     {canvasName: 'canvas_2', coords: [[777, 172], [871, 266], [518, 477], [461, 362]]},
     {canvasName: 'canvas_3', coords: [[304, 134], [549, 147], [550, 567], [312, 578]]}
 ];
+
+// Access Token
 const PERSONAL_ACCESS_TOKEN = '576-3d868804-071a-4d20-a8aa-852d127e3e93';
 
+//Creating link with URL from user
 function getFileKey(pageUrl) {
     const parser = document.createElement('a');
     parser.href = pageUrl;
     return parser.pathname.replace('/file/', '').replace(/\/.*/,'');
 }
 
+
+// Get Figma Frame node-ID
 function getNodeId(pageUrl) {
     const parser = document.createElement('a');
     parser.href = pageUrl;
     return decodeURIComponent(parser.search).replace('?node-id=','');
 }
 
+// Figma API
 function apiRequest(endpoint) {
     return fetch('https://api.figma.com/v1' + endpoint, {
         method: 'GET',
@@ -28,6 +35,7 @@ function apiRequest(endpoint) {
     });
 }
 
+//Adding Figma Frame as image
 function addImageToCanvas(imageUrl) {
     stopProgress();
     const img = new Image();
@@ -46,15 +54,19 @@ function addImageToCanvas(imageUrl) {
     };
 }
 
+
 function callFigmaAndDrawMockups() {
     startProgress();
 
+    // Getting URL from user input
     const pageUrl = document.getElementById('url_input').value;
 
     const nodeId = getNodeId(pageUrl);
 
+    //Using URL from User, asking for access to the Figma frame
     apiRequest('/images/' + getFileKey(pageUrl) + '?ids=' + nodeId)
         .then(function (apiResponse) {
+            // Adding Figma Frame to Canvas
            addImageToCanvas(apiResponse.images[nodeId]);
         });
 }
